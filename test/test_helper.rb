66b0e14a40aed20a0cb4e3e7d9417c50f022d796
sapp <70320848+Egyptianstig@users.noreply.github.com>
2020-08-31 15:54:18 +0000

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,11 +7,26 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
+  #include ApplicationHelper #第5章演習で追加
 
   def is_logged_in?
     !session[:user_id].nil?
   end
 
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
